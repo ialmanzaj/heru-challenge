932e12395ceffb0fdda211ce3bb6e92157fcb113
Isaac Almanza <isaac.almanza19@gmail.com>
2020-11-28 20:11:13 -0500

    feat: adding api

--- api/api.py
+++ api/api.py
@@ -9,14 +9,7 @@ api = Api(app)
 
 class HeruCodeAPI(Resource):
     def post(self):
-        args = self.reqparse.parse_args()
-        text = args['text'].strip()
-        heru = HeruCode(text)
-        return {
-            heru.get_total_prepositions(),
-            heru.get_total_verbs(),
-            heru.get_total_subjunctive_verbs()
-        }, 200
+        pass
 
 
 api.add_resource(HeruCodeAPI, '/parse', endpoint='heru_code')
